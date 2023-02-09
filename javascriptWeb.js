0d0e6571402eb7105d28acff52141e1e9c73deb8
Abdul Rehman S/O M.dildar <abdul44@gmail.com>
2023-02-09 16:28:41 +0500

    Added bg image counting and work on it javascript counter numbering

---
+++ javascriptWeb.js
@@ -0,0 +1,18 @@
+let valuesDisplay = document.querySelectorAll(".num-counting-span-1");
+let interval = 4000;
+
+valuesDisplay.forEach((valuesDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valuesDisplay.getAttribute("data-val"))
+
+    let duration = Math.floor(interval / endValue)
+
+    let counter = setInterval(function () {
+        startValue += 1
+        valuesDisplay.textContent = startValue;
+
+        if (startValue == endValue) {
+            clearInterval(counter)
+        }
+    })
+})
